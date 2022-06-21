9ffa5154422951b9de4570028bdc51fbf7996435
platinum139 <platinum139.dev@gmail.com>
2022-06-21 11:05:38 +0300

    specified host and port for run

--- api.py
+++ api.py
@@ -143,4 +143,4 @@ if __name__ == '__main__':
     if not database_exists(engine.url):
         create_database(engine.url)
     db.create_all()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port='8000')
